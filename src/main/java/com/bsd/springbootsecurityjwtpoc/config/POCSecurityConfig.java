e6f2ef7c290a5260c8487dba7b4f9b6ac3dc0662
siva datta bathina <datta.bathina15@gmail.com>
2019-11-28 22:01:40 +0530

    Docker compose added. .gitignore added.

---
+++ src/main/java/com/bsd/springbootsecurityjwtpoc/config/POCSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.bsd.springbootsecurityjwtpoc.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class POCSecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
