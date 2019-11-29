2da9eddf6fc913fab61534a72fa6451cc0da6f0a
siva datta bathina <datta.bathina15@gmail.com>
2019-11-29 14:52:20 +0530

    Added role array to dummyUser.

--- src/main/java/com/bsd/springbootsecurityjwtpoc/repository/POCSeedDataCommandLineRunner.java
+++ src/main/java/com/bsd/springbootsecurityjwtpoc/repository/POCSeedDataCommandLineRunner.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.bsd.springbootsecurityjwtpoc.domain.Role;
 import com.bsd.springbootsecurityjwtpoc.domain.User;
 import com.bsd.springbootsecurityjwtpoc.exception.UserAlreadyExistsException;
 import com.bsd.springbootsecurityjwtpoc.service.UserService;
@@ -24,12 +25,13 @@ public class POCSeedDataCommandLineRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 //		Create a dummy user object and save it to the user Database.
-		User dummyUser = new User(1, "test", "test", "first name", "last name", "e@mail.com");
+		Role[] roles = {new Role("USER")};
+		User dummyUser = new User(1, "test", "test", "first name", "last name", "e@mail.com", roles);
 		log.debug(dummyUser.toString());
 		try {
 			userService.saveUser(dummyUser);
 		} catch (UserAlreadyExistsException e) {
-			log.error(e.getLocalizedMessage());
+			log.error( e.getClass() + " CommandLineRunner Error.");
 		}
 
 	}
