86a241a388f1d27b26495a27d0995729895c0272
siva datta bathina <datta.bathina15@gmail.com>
2019-11-29 14:51:03 +0530

    ID generation annotation type auto added.

--- src/main/java/com/bsd/springbootsecurityjwtpoc/domain/Role.java
+++ src/main/java/com/bsd/springbootsecurityjwtpoc/domain/Role.java
@@ -1,6 +1,8 @@
 package com.bsd.springbootsecurityjwtpoc.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.validator.constraints.UniqueElements;
@@ -15,7 +17,11 @@ import lombok.NoArgsConstructor;
 @Data
 public class Role {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	@UniqueElements
 	private String role;
+	public Role(@UniqueElements String role) {
+		this.role = role;
+	}
 }
