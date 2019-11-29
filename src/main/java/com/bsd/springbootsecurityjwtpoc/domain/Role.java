106ab8c212b731074e05dbd4bba8cace4764bf48
siva datta bathina <datta.bathina15@gmail.com>
2019-11-29 22:51:07 +0530

    @ManyToMany added for User and Role.

--- src/main/java/com/bsd/springbootsecurityjwtpoc/domain/Role.java
+++ src/main/java/com/bsd/springbootsecurityjwtpoc/domain/Role.java
@@ -1,9 +1,20 @@
 package com.bsd.springbootsecurityjwtpoc.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.UniqueConstraint;
 
 import org.hibernate.validator.constraints.UniqueElements;
 
@@ -19,9 +30,36 @@ public class Role {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
-	@UniqueElements
+	@Column(unique = true)
 	private String role;
-	public Role(@UniqueElements String role) {
+	@ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH })
+	@JoinTable(name = "users_role",
+	joinColumns = @JoinColumn(name = "role_id"),
+	inverseJoinColumns = @JoinColumn(name = "user_id"))
+	private List<User> users;
+	
+	/**
+	 * Constructor that takes in N number of users as argument.
+	 * @param id
+	 * @param role
+	 * @param users
+	 */
+	public Role(int id, @UniqueElements String role, User... users) {
+		super();
+		this.id = id;
 		this.role = role;
+		this.users = Stream.of(users).collect(Collectors.toList());
+	}
+	
+	/**
+	 * Add a single user to the list of users. Check if the users list is null and 
+	 * initializes it if it's null.
+	 * @param user User to be added to the users list.
+	 */
+	public void addUser(User user) {
+		if (users == null) {
+			users = new ArrayList<User>();
+		}
+		users.add(user);
 	}
 }
--- src/main/java/com/bsd/springbootsecurityjwtpoc/domain/User.java
+++ src/main/java/com/bsd/springbootsecurityjwtpoc/domain/User.java
@@ -1,28 +1,35 @@
 package com.bsd.springbootsecurityjwtpoc.domain;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	@NotBlank
+	@Column(unique = true)
 	private String userName;
 	@NotBlank
 	private String password;
@@ -32,6 +39,43 @@ public class User {
 	private String lastName;
 	@NotBlank
 	private String email;
-	@NotBlank
-	private Role[] roles;
+	@ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH })
+	@JoinTable(name = "users_role",
+	joinColumns = @JoinColumn(name = "user_id"),
+	inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles;
+	
+	public User(int id, @NotBlank String userName, @NotBlank String password, @NotBlank String firstName,
+			@NotBlank String lastName, @NotBlank String email, Set<Role> roles) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.roles = roles;
+	}
+	
+	public User(int id, @NotBlank String userName, @NotBlank String password, @NotBlank String firstName,
+			@NotBlank String lastName, @NotBlank String email, Role...roles) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.roles = Stream.of(roles).collect(Collectors.toSet());
+	}
+	
+	
+//	Add role to roles set
+	public void addRole(Role role) {
+		if (roles == null) {
+			roles = new HashSet<Role>();
+		}
+		roles.add(role);
+	}
+	
 }
