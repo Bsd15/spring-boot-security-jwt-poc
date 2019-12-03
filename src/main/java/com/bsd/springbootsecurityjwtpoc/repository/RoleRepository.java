a6fe8abcdb38f726353ff61cca1229f76f03ea47
siva datta bathina <datta.bathina15@gmail.com>
2019-12-03 23:43:27 +0530

    Added existsByRole() method.

--- src/main/java/com/bsd/springbootsecurityjwtpoc/repository/RoleRepository.java
+++ src/main/java/com/bsd/springbootsecurityjwtpoc/repository/RoleRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bsd.springbootsecurityjwtpoc.domain.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+	boolean existsByRole(String role);
 	Optional<Role> findByRole(String role);
 }
