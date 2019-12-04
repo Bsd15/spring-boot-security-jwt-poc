c7bd34971b7209028917ec595c86732c00a77471
siva datta bathina <datta.bathina15@gmail.com>
2019-12-05 00:51:26 +0530

    Added PasswordEncoder Bean and added userDetailsService to auth in configure().

--- src/main/java/com/bsd/springbootsecurityjwtpoc/config/POCSecurityConfig.java
+++ src/main/java/com/bsd/springbootsecurityjwtpoc/config/POCSecurityConfig.java
@@ -1,26 +1,40 @@
 package com.bsd.springbootsecurityjwtpoc.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class POCSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	public POCSecurityConfig(UserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-		auth.inMemoryAuthentication().withUser(userBuilder.username("john").password("test").roles("USER"));
+		auth.userDetailsService(this.userDetailsService);
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/home").hasRole("USER").antMatchers("/").permitAll().and().formLogin();
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }
