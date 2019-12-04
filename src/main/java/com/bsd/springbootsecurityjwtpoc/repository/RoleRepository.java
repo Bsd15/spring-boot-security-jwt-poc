85081950ddd8608bdeab18254ff80f2df17563d0
siva datta bathina <datta.bathina15@gmail.com>
2019-12-04 22:58:19 +0530

    Organized imports.

--- src/main/java/com/bsd/springbootsecurityjwtpoc/repository/RoleRepository.java
+++ src/main/java/com/bsd/springbootsecurityjwtpoc/repository/RoleRepository.java
@@ -1,12 +1,11 @@
 package com.bsd.springbootsecurityjwtpoc.repository;
 
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.bsd.springbootsecurityjwtpoc.domain.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 	boolean existsByRole(String role);
+
 	Optional<Role> findByRole(String role);
 }
