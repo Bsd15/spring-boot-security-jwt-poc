c349892240a88abdccc671150f5b1b270dcba899
siva datta bathina <datta.bathina15@gmail.com>
2019-12-04 23:00:57 +0530

    Organized imports.

--- src/main/java/com/bsd/springbootsecurityjwtpoc/domain/Role.java
+++ src/main/java/com/bsd/springbootsecurityjwtpoc/domain/Role.java
@@ -1,22 +1,10 @@
 package com.bsd.springbootsecurityjwtpoc.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.UniqueConstraint;
-
-import org.hibernate.validator.constraints.UniqueElements;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
