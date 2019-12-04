ee67fed9b6deb5da1d04f49c703a9718b48628cb
siva datta bathina <datta.bathina15@gmail.com>
2019-12-04 22:59:39 +0530

    Changed findByUserName() to use a JOIN FETCH query to avoid LazyFetchInitialization Exception.

--- src/main/java/com/bsd/springbootsecurityjwtpoc/repository/UserRepository.java
+++ src/main/java/com/bsd/springbootsecurityjwtpoc/repository/UserRepository.java
@@ -3,9 +3,13 @@ package com.bsd.springbootsecurityjwtpoc.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.bsd.springbootsecurityjwtpoc.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-	Optional<User> findByUserName(String userName);
+	@Query(value = "SELECT u FROM User u JOIN FETCH u.roles where u.userName = :userName")
+	Optional<User> findByUserName(@Param("userName") String userName);
+//	Optional<User> findByUserName(String userName);
 }
