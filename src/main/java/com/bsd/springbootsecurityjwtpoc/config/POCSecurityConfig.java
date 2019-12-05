ab9c211f542a65a42defd3ee6e3d83a19d946ff8
siva datta bathina <datta.bathina15@gmail.com>
2019-12-05 22:31:04 +0530

    Added BCrypt password encoder to security config.

--- src/main/java/com/bsd/springbootsecurityjwtpoc/config/POCSecurityConfig.java
+++ src/main/java/com/bsd/springbootsecurityjwtpoc/config/POCSecurityConfig.java
@@ -3,11 +3,13 @@ package com.bsd.springbootsecurityjwtpoc.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -25,6 +27,7 @@ public class POCSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(this.userDetailsService);
+		auth.authenticationProvider(getAuthenticationProvider());
 	}
 
 	@Override
@@ -32,9 +35,27 @@ public class POCSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers("/home").hasRole("USER").antMatchers("/").permitAll().and().formLogin();
 	}
 	
+	/**
+	 * Creates a bean for BCryptPasswordEncoder
+	 * @return BCryptPasswordEncoder
+	 */
 	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+	public BCryptPasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	/**
+	 * Creates a bean for DaoAuthenticationProvider with UserDetailsService
+	 * and PasswordEncoder(BCryptPasswordEncoder) set.
+	 * @return DaoAuthenticationProvider
+	 */
+	@Bean
+	public DaoAuthenticationProvider getAuthenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userDetailsService);
+		authenticationProvider.setPasswordEncoder(getPasswordEncoder());
+		return authenticationProvider;
+		
 	}
 
 }
