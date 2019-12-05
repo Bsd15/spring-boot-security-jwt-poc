3487c14fe2a8082aecfae6412e33edd5ad8b3319
siva datta bathina <datta.bathina15@gmail.com>
2019-12-06 00:50:52 +0530

    Changed configure(), added "/authenticate" to handler.

--- src/main/java/com/bsd/springbootsecurityjwtpoc/config/POCSecurityConfig.java
+++ src/main/java/com/bsd/springbootsecurityjwtpoc/config/POCSecurityConfig.java
@@ -3,6 +3,7 @@ package com.bsd.springbootsecurityjwtpoc.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -32,7 +33,7 @@ public class POCSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/home").hasRole("USER").antMatchers("/").permitAll().and().formLogin();
+		http.authorizeRequests().antMatchers("/home").hasRole("USER").antMatchers("/authenticate").permitAll().and().formLogin();
 	}
 	
 	/**
@@ -55,7 +56,11 @@ public class POCSecurityConfig extends WebSecurityConfigurerAdapter {
 		authenticationProvider.setUserDetailsService(userDetailsService);
 		authenticationProvider.setPasswordEncoder(getPasswordEncoder());
 		return authenticationProvider;
-		
+	}
+	
+	@Bean
+	public AuthenticationManager getAuthenticationManager() throws Exception {
+		return super.authenticationManager();
 	}
 
 }
